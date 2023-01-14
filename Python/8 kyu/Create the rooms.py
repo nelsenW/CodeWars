38f6159892f439df7e315c2f850bcc962342d00a
nelsenw <wnelsen9@gmail.com>
2023-01-14 11:25:19 -0800

    py day 2

---
+++ Python/8 kyu/Create the rooms.py
@@ -0,0 +1,17 @@
+rooms = {
+    1:{
+        "name": "1",
+        "description": "true",
+        "completed": "false"
+    },
+    2:{
+        "name": "1",
+        "description": "true",
+        "completed": "false" 
+    },
+    3:{
+        "name": "1",
+        "description": "true",
+        "completed": "false"
+    }
+}
