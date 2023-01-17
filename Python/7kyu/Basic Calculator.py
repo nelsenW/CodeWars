70f18f46b5222ae2c721021d76a650409ba3b677
nelsenw <wnelsen9@gmail.com>
2023-01-17 14:57:04 -0800

    Py 5:27

---
+++ Python/7kyu/Basic Calculator.py
@@ -0,0 +1,13 @@
+def calculate(num1, operation, num2): 
+    if operation not in "+-/*":
+        return None
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    else:
+        if num2 == 0:
+            return None
+        return num1 / num2
